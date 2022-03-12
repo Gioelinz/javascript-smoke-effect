3b5ae6efd1a8fb8689762db66ab844481250b87a
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-12 15:50:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = document.querySelector('.text');
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+
+const letters = document.querySelectorAll('span');
+
+for (let i = 0; i < letters.length; i++) {
+    const letter = letters[i];
+
+    letter.addEventListener("mouseover", function () {
+        letter.classList.add('active');
+    })
+}
